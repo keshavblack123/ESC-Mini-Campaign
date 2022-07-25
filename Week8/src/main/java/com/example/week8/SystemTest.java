f376a715e9b362a2782d34656a59b1a1f1928a42
keshavblack123 <keshavnatarajan@gmail.com>
2022-07-25 19:45:30 +0800

    test file added

--- Week8/src/main/java/com/example/week8/SystemTest.java
+++ Week8/src/main/java/com/example/week8/SystemTest.java
@@ -1,120 +1,100 @@
-package com.example.week8;
-import org.junit.Test;
-import java.io.PrintWriter;
-import java.util.Scanner;
-import static org.junit.Assert.*;
-
-public class SystemTest {
-
-    String globalPath = "/Users/keshavnatarajan/Documents/GitHub/ESC-Mini-Campaign/TestFiles/SystemTests/";
-
-
-
-    @Test
-    public void testSingleMismatch(){
-        String path1 = globalPath + "testSingleMismatch/singleMismatch1.csv";
-        String path2 = globalPath + "testSingleMismatch/singleMismatch2.csv";
-        String outPath = globalPath + "testSingleMismatch/singleMismatchOutput.csv";
-
-        PrintWriter writer = CSVCompare.getWriter(outPath);
-        assertTrue(CSVCompare.cleanFiles(path1, path2)); //Expected output to pass
-        CSVCompare.compareLines(path1, path2, writer); //Expected output "Passed"
-    }
-
-    @Test
-    public void testAllMismatch(){
-        String path1 = globalPath + "testAllMismatch/allMismatch1.csv";
-        String path2 = globalPath + "testAllMismatch/allMismatch2.csv";
-        String outPath = globalPath + "testAllMismatch/allMismatchOutput.csv";
-
-        Scanner parser1 = CSVCompare.getParser(path1);
-        Scanner parser2 = CSVCompare.getParser(path2);
-        PrintWriter writer = CSVCompare.getWriter(outPath);
-
-        assertTrue(CSVCompare.cleanFiles(path1, path2)); //Expected output to pass
-
-        CSVCompare.compareLines(path1, path2, writer); //Expected output "Passed"
-    }
-
-    @Test
-    public void testIdentical(){
-        String path1 = globalPath + "testIdentical/identical1.csv";
-        String path2 = globalPath + "testIdentical/identical2.csv";
-        String outPath = globalPath + "testIdentical/identicalOutput.csv";
-
-        Scanner parser1 = CSVCompare.getParser(path1);
-        Scanner parser2 = CSVCompare.getParser(path2);
-        PrintWriter writer = CSVCompare.getWriter(outPath);
-
-        assertTrue(CSVCompare.cleanFiles(path1, path2)); //Expected output to pass
-
-        CSVCompare.compareLines(path1, path2, writer); //Expected output "Passed"
-        //Empty output file will be produced
-    }
-
-    @Test
-    public void testOneOutOfOrder(){
-        String path1 = globalPath + "testOneOutOfOrder/oneOutOfOrder1.csv";
-        String path2 = globalPath + "testOneOutOfOrder/oneOutOfOrder1.csv";
-        String outPath = globalPath + "testOneOutOfOrder/oneOutOfOrderOutput.csv";
-
-        Scanner parser1 = CSVCompare.getParser(path1);
-        Scanner parser2 = CSVCompare.getParser(path2);
-        PrintWriter writer = CSVCompare.getWriter(outPath);
-
-        assertFalse(CSVCompare.cleanFiles(path1, path2)); //Expected output to fail
-
-        CSVCompare.compareLines(path1, path2, writer); //Expected output "Failed"
-        //Error in file 1, No output CSV created
-    }
-
-    @Test
-    public void testAllOutOfOrder(){
-        String path1 = globalPath + "testAllOutOfOrder/allOutOfOrder1.csv";
-        String path2 = globalPath + "testAllOutOfOrder/allOutOfOrder1.csv";
-        String outPath = globalPath + "testAllOutOfOrder/allOutOfOrderOutput.csv";
-
-        Scanner parser1 = CSVCompare.getParser(path1);
-        Scanner parser2 = CSVCompare.getParser(path2);
-        PrintWriter writer = CSVCompare.getWriter(outPath);
-
-        assertFalse(CSVCompare.cleanFiles(path1, path2)); //Expected output to fail
-
-        CSVCompare.compareLines(path1, path2, writer); //Expected output "Failed"
-        //Error in file 2, No output CSV created
-    }
-
-    @Test
-    public void testMissingAccount(){
-        String path1 = globalPath + "testMissingAccounts/MissingAccount1.csv";
-        String path2 = globalPath + "testMissingAccounts/MissingAccount1.csv";
-        String outPath = globalPath + "testMissingAccounts/MissingAccountOutput.csv";
-
-        Scanner parser1 = CSVCompare.getParser(path1);
-        Scanner parser2 = CSVCompare.getParser(path2);
-        PrintWriter writer = CSVCompare.getWriter(outPath);
-
-        assertFalse(CSVCompare.cleanFiles(path1, path2)); //Expected output to fail
-
-        CSVCompare.compareLines(path1, path2, writer); //Expected output "Failed"
-        //Error in file 1, No output CSV created
-    }
-
-    @Test
-    public void testAdditionalAccount(){
-        String path1 = globalPath + "testAdditionalAccounts/AdditionalAccounts1.csv";
-        String path2 = globalPath + "testAdditionalAccounts/AdditionalAccounts2.csv";
-        String outPath = globalPath + "testAdditionalAccounts/AdditionalAccountsOutput.csv";
-
-        Scanner parser1 = CSVCompare.getParser(path1);
-        Scanner parser2 = CSVCompare.getParser(path2);
-        PrintWriter writer = CSVCompare.getWriter(outPath);
-
-        assertFalse(CSVCompare.cleanFiles(path1, path2)); //Expected output to fail
-
-        CSVCompare.compareLines(path1, path2, writer); //Expected output "Failed"
-        //Error in file 2, No output CSV created
-    }
-
-
-}
+//package com.example.week8;
+//
+//import org.junit.Test;
+//import java.io.PrintWriter;
+//import static org.junit.Assert.*;
+//
+//public class SystemTest {
+//
+//    String globalPath = "/Users/keshavnatarajan/Documents/GitHub/ESC-Mini-Campaign/TestFiles/SystemTests/";
+//
+//    @Test
+//    public void testSingleMismatch(){
+//        String path1 = globalPath + "testSingleMismatch/singleMismatch1.csv";
+//        String path2 = globalPath + "testSingleMismatch/singleMismatch2.csv";
+//        String outPath = globalPath + "testSingleMismatch/singleMismatchOutput.csv";
+//
+//        PrintWriter writer = CSVCompare.getWriter(outPath);
+//        assertTrue(CSVCompare.cleanFiles(path1, path2)); //Expected output to pass
+//        CSVCompare.compareLines(path1, path2, writer); //Expected output "Passed"
+//    }
+//
+//    @Test
+//    public void testAllMismatch(){
+//        String path1 = globalPath + "testAllMismatch/allMismatch1.csv";
+//        String path2 = globalPath + "testAllMismatch/allMismatch2.csv";
+//        String outPath = globalPath + "testAllMismatch/allMismatchOutput.csv";
+//
+//        PrintWriter writer = CSVCompare.getWriter(outPath);
+//
+//        assertTrue(CSVCompare.cleanFiles(path1, path2)); //Expected output to pass
+//        CSVCompare.compareLines(path1, path2, writer); //Expected output "Passed"
+//    }
+//
+//    @Test
+//    public void testIdentical(){
+//        String path1 = globalPath + "testIdentical/identical1.csv";
+//        String path2 = globalPath + "testIdentical/identical2.csv";
+//        String outPath = globalPath + "testIdentical/identicalOutput.csv";
+//
+//        PrintWriter writer = CSVCompare.getWriter(outPath);
+//
+//        assertTrue(CSVCompare.cleanFiles(path1, path2)); //Expected output to pass
+//        CSVCompare.compareLines(path1, path2, writer); //Expected output "Passed"
+//        //Empty output file will be produced
+//    }
+//
+//    @Test
+//    public void testOneOutOfOrder(){
+//        String path1 = globalPath + "testOneOutOfOrder/oneOutOfOrder1.csv";
+//        String path2 = globalPath + "testOneOutOfOrder/oneOutOfOrder1.csv";
+//        String outPath = globalPath + "testOneOutOfOrder/oneOutOfOrderOutput.csv";
+//
+//        PrintWriter writer = CSVCompare.getWriter(outPath);
+//
+//        assertFalse(CSVCompare.cleanFiles(path1, path2)); //Expected output to fail
+//        CSVCompare.compareLines(path1, path2, writer); //Expected output "Failed"
+//        //Error in file 1, No output CSV created
+//    }
+//
+//    @Test
+//    public void testAllOutOfOrder(){
+//        String path1 = globalPath + "testAllOutOfOrder/allOutOfOrder1.csv";
+//        String path2 = globalPath + "testAllOutOfOrder/allOutOfOrder1.csv";
+//        String outPath = globalPath + "testAllOutOfOrder/allOutOfOrderOutput.csv";
+//
+//        PrintWriter writer = CSVCompare.getWriter(outPath);
+//
+//        assertFalse(CSVCompare.cleanFiles(path1, path2)); //Expected output to fail
+//        CSVCompare.compareLines(path1, path2, writer); //Expected output "Failed"
+//        //Error in file 2, No output CSV created
+//    }
+//
+//    @Test
+//    public void testMissingAccount(){
+//        String path1 = globalPath + "testMissingAccounts/MissingAccount1.csv";
+//        String path2 = globalPath + "testMissingAccounts/MissingAccount1.csv";
+//        String outPath = globalPath + "testMissingAccounts/MissingAccountOutput.csv";
+//
+//        PrintWriter writer = CSVCompare.getWriter(outPath);
+//
+//        assertFalse(CSVCompare.cleanFiles(path1, path2)); //Expected output to fail
+//        CSVCompare.compareLines(path1, path2, writer); //Expected output "Failed"
+//        //Error in file 1, No output CSV created
+//    }
+//
+//    @Test
+//    public void testAdditionalAccount(){
+//        String path1 = globalPath + "testAdditionalAccounts/AdditionalAccounts1.csv";
+//        String path2 = globalPath + "testAdditionalAccounts/AdditionalAccounts2.csv";
+//        String outPath = globalPath + "testAdditionalAccounts/AdditionalAccountsOutput.csv";
+//
+//        PrintWriter writer = CSVCompare.getWriter(outPath);
+//
+//        assertFalse(CSVCompare.cleanFiles(path1, path2)); //Expected output to fail
+//        CSVCompare.compareLines(path1, path2, writer); //Expected output "Failed"
+//        //Error in file 2, No output CSV created
+//    }
+//
+//
+//}
---
+++ Week8/src/test/java/UnitTest.java
@@ -0,0 +1,79 @@
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import com.example.week8.CSVCompare;
+import org.junit.Test;
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class UnitTest {
+    String globalPath = "/Users/keshavnatarajan/Documents/GitHub/ESC-Mini-Campaign/TestFiles/UnitTests/";
+
+    @Test
+    public void testGetParser(){
+        System.out.println("Testing");
+        String path = globalPath + "testGetParser.csv";
+        Scanner parser = CSVCompare.getParser(path);
+        //Fails if exception is thrown (file does not exist)
+    }
+
+    @Test
+    public void testGetWriter(){
+        String path = globalPath + "outputFile.csv";
+        PrintWriter writer = CSVCompare.getWriter(path);
+        //Fails if exception is thrown
+    }
+
+    @Test
+    public void testCheckOrderedAccounts(){
+        String path = globalPath + "testOrderedAccounts.csv";
+        Scanner parser = CSVCompare.getParser(path);
+        Boolean out = CSVCompare.AccountsOrdered(parser);
+        assertTrue(out);
+    }
+
+    @Test
+    public void testCheckUnorderedAccounts(){
+        String path = globalPath + "testUnorderedAccounts.csv";
+        Scanner parser = CSVCompare.getParser(path);
+        Boolean out = CSVCompare.AccountsOrdered(parser);
+        assertFalse(out);
+    }
+
+    @Test
+    public void testCheckExtraLines1(){
+        String path1 = globalPath + "test1AdditionalAccount/additionalAccount1.csv";
+        String path2 = globalPath + "test1AdditionalAccount/additionalAccount2.csv";
+        Scanner parser1 = CSVCompare.getParser(path1);
+        Scanner parser2 = CSVCompare.getParser(path2);
+        Boolean out = CSVCompare.checkExtraLines(parser1, parser2);
+        assertFalse(out);
+    }
+
+    @Test
+    public void testCheckExtraLinesMany(){
+        String path1 = globalPath + "testManyAdditionalAccount/manyAdditions1.csv";
+        String path2 = globalPath + "testManyAdditionalAccount/manyAdditions2.csv";
+        Scanner parser1 = CSVCompare.getParser(path1);
+        Scanner parser2 = CSVCompare.getParser(path2);
+        Boolean out = CSVCompare.checkExtraLines(parser1, parser2);
+        assertFalse(out);
+    }
+
+    @Test
+    public void testCheckNoExtraLines(){
+        String path1 = globalPath + "testNoExtraAccounts/NoExtra1.csv";
+        String path2 = globalPath + "testNoExtraAccounts/NoExtra2.csv";
+        Scanner parser1 = CSVCompare.getParser(path1);
+        Scanner parser2 = CSVCompare.getParser(path2);
+        Boolean out = CSVCompare.checkExtraLines(parser1, parser2);
+        assertTrue(out);
+    }
+
+    @Test
+    public void testCountLine(){
+        String path = globalPath + "testCountLine.csv";
+        Scanner parser = CSVCompare.getParser(path);
+        int lines = CSVCompare.countLines(parser);
+        assertTrue(lines == 24);
+    }
+}
